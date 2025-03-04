8d43bfcdbf341b1a0391357b80859fed23627dd8
Filipe <frtcorreia@gmail.com>
2025-03-04 16:04:43 +0000

    remove old name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
             <div className="flex items-center">
               <Calendar className="h-8 w-8 text-indigo-400" />
               <span className="ml-2 text-xl font-bold text-white">
-                VacationSync
+                Holiflow
               </span>
             </div>
             <p className="text-gray-300 text-base">{t("footer.description")}</p>
